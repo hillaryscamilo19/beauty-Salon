c0c09495ee262a6783b166c4d984401ce58a4a4b
hillarys Camilo <Hcamilo@ssv.local>
2025-04-03 17:53:29 -0400

    cambios

--- src/app/modules/dashboard/profile/profile.component.ts
+++ src/app/modules/dashboard/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthServiceService } from 'src/app/services/servicesAuth/auth-service.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+
+  constructor(private authService: AuthServiceService,private router: Router){}
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 
 }
